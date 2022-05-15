68c00582c0cf8457067683bc2b50a9ee3ccf8ca5
0xLeif <zmeriksen@icloud.com>
2022-05-15 10:09:07 -0600

    Resolve merge issues

--- Sources/CacheStore/ActionHandling.swift
+++
@@ -1,8 +0,0 @@
-public protocol ActionHandling {
-    associatedtype Action
-    
-    /// Handle the given `Action`
-    func handle(action: Action)
-}
-
-public typealias StateActionHandling<Key: Hashable, Action, Dependency> = (inout CacheStore<Key>, Action, Dependency) -> Void
