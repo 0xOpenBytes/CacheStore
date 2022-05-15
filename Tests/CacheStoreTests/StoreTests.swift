10da3496c66b09658a38198cdb304a363d24d9af
0xLeif <zmeriksen@icloud.com>
2022-05-15 10:07:35 -0600

    Update Tests, README, and files

---
+++ Tests/CacheStoreTests/StoreTests.swift
@@ -0,0 +1,39 @@
+import t
+import c
+import XCTest
+@testable import CacheStore
+
+class StoreTests: XCTestCase {
+    func testExample() {
+        XCTAssert(
+            t.suite(named: "Testing Store") {
+enum StoreKey {
+    case isOn
+}
+
+enum Action {
+    case toggle
+}
+
+let actionHandler = StoreActionHandler<StoreKey, Action, Void> { (store: inout CacheStore<StoreKey>, action: Action, _: Void) in
+    switch action {
+    case .toggle:
+        store.update(.isOn, as: Bool.self, updater: { $0?.toggle() })
+    }
+}
+
+let store = Store<StoreKey, Action, Void>(
+    initialValues: [.isOn: false],
+    actionHandler: actionHandler,
+    dependency: ()
+)
+
+try t.assert(store.get(.isOn), isEqualTo: false)
+
+store.handle(action: .toggle)
+
+try t.assert(store.get(.isOn), isEqualTo: true)
+            }
+        )
+    }
+}
