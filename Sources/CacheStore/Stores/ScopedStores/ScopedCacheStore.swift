b61a54593a3776054f8265dfa6233f4732514e2f
0xLeif <zmeriksen@icloud.com>
2022-05-18 20:12:27 -0600

    Add ScopedCacheStore copy()

--- Sources/CacheStore/Stores/ScopedStores/ScopedCacheStore.swift
+++ Sources/CacheStore/Stores/ScopedStores/ScopedCacheStore.swift
@@ -2,7 +2,7 @@ import c
 
 class ScopedCacheStore<Key: Hashable, ScopedKey: Hashable>: CacheStore<ScopedKey> {
     weak var parentCacheStore: CacheStore<Key>?
-    private var keyTransformation: c.BiDirectionalTransformation<Key?, ScopedKey?>?
+    private var keyTransformation: c.BiDirectionalTransformation<Key?, ScopedKey?>
     
     init(
         keyTransformation: c.BiDirectionalTransformation<Key?, ScopedKey?>
@@ -12,18 +12,22 @@ class ScopedCacheStore<Key: Hashable, ScopedKey: Hashable>: CacheStore<ScopedKey
         super.init(initialValues: [:])
     }
     
-    required init(initialValues: [ScopedKey: Any]) {
-        super.init(initialValues: initialValues)
-    }
+    required init(initialValues: [ScopedKey: Any]) { fatalError("Not implemented") }
     
     override func set<Value>(value: Value, forKey key: ScopedKey) {
         super.set(value: value, forKey: key)
         
-        guard
-            let keyTransformation = keyTransformation,
-            let parentKey = keyTransformation.to(key)
-        else { return }
+        guard let parentKey = keyTransformation.to(key) else { return }
         
         parentCacheStore?.set(value: value, forKey: parentKey)
+    }
+    
+    override func copy() -> ScopedCacheStore<Key, ScopedKey> {
+        let scopedCacheStore = ScopedCacheStore<Key, ScopedKey>(keyTransformation: keyTransformation)
+        
+        scopedCacheStore.cache = cache
+        scopedCacheStore.parentCacheStore = parentCacheStore
+        
+        return scopedCacheStore
     }
 }
