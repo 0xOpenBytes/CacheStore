f30227dd5504b6fd8d3941dc64898a228f931300
Leif <zmeriksen@icloud.com>
2022-10-24 18:29:51 -0600

    Add StoreContentTests

--- Tests/CacheStoreTests/StoreContentTests.swift
+++ Tests/CacheStoreTests/StoreContentTests.swift
@@ -6,30 +6,41 @@
 //
 
 import XCTest
+@testable import CacheStore
 
 final class StoreContentTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testBasicContent() throws {
+        enum Key {
+            case text
         }
-    }
 
+        enum Action {
+            case updateText(String)
+        }
+
+        let store = Store<Key, Action, Void>(
+            initialValues: [:],
+            actionHandler: StoreActionHandler<Key, Action, ()> { cacheStore, action, _ in
+                switch action {
+                case let .updateText(text):
+                    cacheStore.set(value: text, forKey: .text)
+                    return .none
+                }
+            }
+        )
+
+        struct ViewContent: StoreContent {
+            let text: String
+
+            init(store: Store<Key, Void, Void>) {
+                text = store.get(.text) ?? "Hello, World"
+            }
+        }
+
+        XCTAssertEqual(store.content(using: ViewContent.self).text, "Hello, World")
+
+        store.handle(action: .updateText("Updated!"))
+
+        XCTAssertEqual(store.content(using: ViewContent.self).text, "Updated!")
+    }
 }
