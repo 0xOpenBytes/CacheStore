d04c4b8e592fac2a52da4619152270d0dae10603
Leif <zmeriksen@icloud.com>
2022-11-09 18:12:42 -0700

    Enforce weak on actionHandler scope

--- Sources/CacheStore/Stores/Store/Store.swift
+++ Sources/CacheStore/Stores/Store/Store.swift
@@ -219,7 +219,7 @@ open class Store<Key: Hashable, Action, Dependency>: ObservableObject, ActionHan
 
         scopedStore.cacheStore = scopedCacheStore
         scopedStore.parentStore = self
-        scopedStore.actionHandler = StoreActionHandler { [weak scopedStore] (store: inout CacheStore<ScopedKey>, action: ScopedAction, dependency: ScopedDependency) in
+        scopedStore.actionHandler = StoreActionHandler { [weak scopedStore, actionHandler] (store: inout CacheStore<ScopedKey>, action: ScopedAction, dependency: ScopedDependency) in
             let effect = actionHandler.handle(store: &store, action: action, dependency: dependency)
 
             if let parentAction = actionTransformation(action) {
